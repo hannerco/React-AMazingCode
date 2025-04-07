d2d7f49cb3f6ae256e66a0a35048fbffc936b93d
Hanner Conde <160528575+hannerco@users.noreply.github.com>
2025-04-07 11:53:05 -0500

    Renderizado de listas de OBJETOS

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,35 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Menu from './components/Menu'
 
-const primerCirculo = ["Nela", "Levette"]
+const primerCirculo = [
+  {
+    id: 1,
+    name: 'Lourdes Valentina Levette Ipuana',
+    age: 23,
+    nickName: 'Levi',
+  },
 
-const HTMLFriends = primerCirculo.map((friend, index) => {
+  {
+    id: 2,
+    name: 'Lauren Daniela Cruz Ladino',
+    age: 20,
+    nickName: 'Nela',
+  }
+]
+
+const HTMLFriends = primerCirculo.map((friend) => {
   return ( 
-    <p key={friend}>{index} | {friend}</p>
+    // Es importante que cada elemento tenga una key que lo identifique
+    <div key={id}> 
+
+      <h2>Nombre: {friend.name}</h2>
+      <p>Edad: {friend.age}</p>
+      <p>Apodo: {friend.nickName}</p>
+    </div>
   )
 })
 
 function App() {
-  const condition = true;
 
   return (
     <>
