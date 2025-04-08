3b834ad765b090857fc23bb27795a3754b8dac35
Hanner Conde <160528575+hannerco@users.noreply.github.com>
2025-04-08 11:31:48 -0500

    Fetch API | Alone

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,38 @@
-import {useEffect, useState} from 'react';
-import Child from './components/Child';
+import React, { useState, useEffect } from 'react'
 
-function App(){
-  //Variable que almacena la info de la API
-  //Es useState porque va a cambiar, y ese cambio se debe ver en la página
-  const [user, setUser] = useState([])
 
-  // Aquí llamamos a la API y pedimos los datos
+const App = () => {
+  const [users, setUsers] = useState([]);
+
   const getUsers = async () => {
-    const response = await fetch("https://jsonplaceholder.typicode.com/users")
+    const response = await fetch("https://jsonplaceholder.typicode.com/users");
     const data = await response.json();
-    setUser(data)
-    //Ahora user tiene toda la info de la API en su array
+    setUsers(data)
   }
 
-
-
-  // Se llamará getUsers cada que se monte el componente
-  useEffect(()=> {
-    getUsers();
+  useEffect(() => {
+    getUsers()
   }, [])
 
-  /** Si de repente hay un nuevo usuario, 
-   * habría que recargar la página para verlo 
-   * (haciendo que se monte el componente de nuevo)
-   * o crear un botón que llame a getUsers
-  */
-
-
-
-  const HTMLUsers = user.map((user) => {
-    return (
-      <div key={user.id}>
-        <h3>{user.name}</h3>
+  const HTMLShowUsers = users.map(
+    (u) => {
+      return (
+      <div key={u.id}>
+        <h2>{u.name}</h2>
+        <h3>{u.email}</h3>
       </div>
-    )
-  })
-
+      )
+    }
+  )
 
 
   return (
     <div>
-      <h1>fetch() | Llamada a una API</h1>
-      <section>{HTMLUsers}</section>
+      <h1>Todos</h1>
+      <section>{HTMLShowUsers}</section>
+
     </div>
   )
-
-
 }
 
-
-
-export default App;
+export default App
