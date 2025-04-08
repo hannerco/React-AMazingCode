e059d23b6cc5d157e3b8843a507238abb318703c
Hanner Conde <160528575+hannerco@users.noreply.github.com>
2025-04-08 12:22:29 -0500

    Formularios React

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,13 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
+import Formulario from './components/Formulario'
 
 
 const App = () => {
-  const [users, setUsers] = useState([]);
-
-  const getUsers = async () => {
-    const response = await axios.get("https://jsonplaceholder.typicode.com/users");
-    console.log(response)
-    setUsers(response.data)
-  }
-
-  useEffect(() => {
-    getUsers()
-  }, [])
-
-  const HTMLShowUsers = users.map(
-    (u) => {
-      return (
-      <div key={u.id}>
-        <h2>{u.name}</h2>
-        <h3>{u.email}</h3>
-      </div>
-      )
-    }
-  )
-
-
+  
+  
   return (
-    <div>
-      <h1>Axios | LLamada a una API</h1>
-      <section>{HTMLShowUsers}</section>
-
-    </div>
+    <Formulario></Formulario>
   )
 }
 
