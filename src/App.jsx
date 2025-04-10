0ef2e20f2631dd6bc04a3293b2a7ada2c60517d6
Hanner Conde <160528575+hannerco@users.noreply.github.com>
2025-04-10 11:42:17 -0500

    React-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
 import React, { useState, useEffect } from 'react'
+import {Route, Routes} from 'react-router-dom'
 import axios from 'axios'
 import Formulario from './components/Formulario'
 import FormularioPropio from './components/FormularioPropio'
+import HomePage from './pages/HomePage'
+import BlogPage from './pages/BlogPage'
 
 
 const App = () => {
   
   
   return (
-    <>
-    
-    <Formulario></Formulario>
-
-    {/* <FormularioPropio></FormularioPropio> */}
-    </>
+    <Routes>
+      <Route path='/' element={<HomePage/>}/>
+      <Route path='/blog' element={<BlogPage/>}/>
+      
+    </Routes>
   )
 }
 
